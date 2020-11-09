b2153680c63ea7ee8e89bf85a4ae5006f7c5e41d
Jeffrey Chesanek <jchesanek513@g.rwu.edu>
2020-11-08 21:07:22 -0500

    Added Timer + Game Over message (temporary)

---
+++ Ultimate Ultimate Air Hockey/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public Text timeText;
+    public GameObject gameOverText;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+        gameOverText.SetActive(false);
+        DisplayTime(timeRemaining);
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                gameOverText.SetActive(true);
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText = GameObject.Find("TimeText").GetComponent<Text>();
+        timeText.text = "Time: " + string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
