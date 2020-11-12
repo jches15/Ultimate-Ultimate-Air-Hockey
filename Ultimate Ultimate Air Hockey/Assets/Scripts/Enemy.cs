6a99eab80fde25413adf0b2fb93b671bd39ca840
Jeffrey Chesanek <jchesanek513@g.rwu.edu>
2020-11-12 15:24:31 -0500

    Updated Enemy Movement, more randomness when player hits puck

--- Ultimate Ultimate Air Hockey/Assets/Scripts/Enemy.cs
+++ Ultimate Ultimate Air Hockey/Assets/Scripts/Enemy.cs
@@ -9,21 +9,28 @@ public class Enemy : MonoBehaviour
   private Vector2 startingPosition;
   public Rigidbody2D Puck;
 
+  public Transform thePuck;
+
   private Vector2 targetPosition; 
  // public Transform PlayerBoundaryHolder; 
  private void Start()
  {
      rb = GetComponent<Rigidbody2D>();
-     startingPosition = rb.position;
+     //startingPosition = rb.position;
+     startingPosition = transform.position;
 
  }
  private void FixedUpdate()
  {
      float movementSpeed;
 
-    //  movementSpeed = MaxMovementSpeed * Random.Range(0.1f, 0.3f);
-     movementSpeed = Random.Range(MaxMovementSpeed * 0.4f, MaxMovementSpeed);
-    rb.MovePosition(Vector2.MoveTowards(rb.position, targetPosition, movementSpeed * Time.fixedDeltaTime));
+      movementSpeed = MaxMovementSpeed * Random.Range(0.1f, 0.3f);
+     //movementSpeed = Random.Range(MaxMovementSpeed * 0.4f, MaxMovementSpeed);
+    //rb.MovePosition(Vector2.MoveTowards(rb.position, targetPosition, movementSpeed * Time.fixedDeltaTime));
+
+    targetPosition.x = thePuck.position.x;
+    targetPosition.y = thePuck.position.y;
+    transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementSpeed * Time.fixedDeltaTime);
  }
 
 
--- Ultimate Ultimate Air Hockey/Assets/Scripts/puck.cs
+++ Ultimate Ultimate Air Hockey/Assets/Scripts/puck.cs
@@ -7,6 +7,8 @@ public class puck : MonoBehaviour
     Rigidbody2D rb;
     //GameObject puckk;
     float speed = 55;
+    public Transform player;
+    public float ydirPuck;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,21 @@ public class puck : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D col){
         if(col.gameObject.tag == "player"){
-                rb.AddForce(new Vector2(speed, 10), ForceMode2D.Impulse);
+                Vector2 dir = player.position - transform.position;
+                if(dir.y > 0){ //ball is below player
+                    ydirPuck = Random.Range(-1, -20);
+                    Debug.Log(ydirPuck);
+                    rb.AddForce(new Vector2(speed, ydirPuck), ForceMode2D.Impulse);
+                }
+                else if(dir.y == 0){ //if puck is right in front of player (unlikely)
+                    rb.AddForce(new Vector2(speed, 0), ForceMode2D.Impulse);
+                }
+                else{ //ball is above player
+                    ydirPuck = Random.Range(1, 20);
+                    Debug.Log(ydirPuck);
+                    rb.AddForce(new Vector2(speed, ydirPuck), ForceMode2D.Impulse);
+                }
+                //rb.AddForce(new Vector2(speed, 10), ForceMode2D.Impulse);
         }
     }
 }
