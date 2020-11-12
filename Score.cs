f7196bceed8f6a1758687b3af6c0566764aa5924
bdarelius246 <60321238+bdarelius246@users.noreply.github.com>
2020-11-12 17:38:44 -0500

    I worked on the scoreboard and blue line boundary
    I keep getting an error every time the puck enters the goal. Still needs work

---
+++ Score.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public enum Scores
+    {
+        AiScore, PlayerScore
+
+
+
+    }
+ public Text AiScoreTxt, PlayerScoreTxt;
+ private int aiScore, playerScore;
+
+public void Increment(Scores whichScore)
+{
+    if(whichScore == Scores.AiScore)
+        AiScoreTxt.text = (++aiScore).ToString();
+    else
+        PlayerScoreTxt.text = (++playerScore).ToString();
+
+
+
+
+}
+
+
+
+}
